d5a649840055034273dd1b06965dba587d416554
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-04 21:03:53 +0900

    add: project pamajon

---
+++ pamajon/src/main/java/com/pamajon/pamajon/PamajonApplication.java
@@ -0,0 +1,13 @@
+package com.pamajon.pamajon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamajonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamajonApplication.class, args);
+	}
+
+}
---
+++ pamajon/src/test/java/com/pamajon/pamajon/PamajonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamajon.pamajon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamajonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
