400c9cadcccf7865049955a11415cdde0098a0ff
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-08 10:10:36 +0900

    add: MariaDB DBMS connected 1/8

---
+++ pamajon/src/main/java/com/pamajon/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.pamajon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/main")
+    public String main(){
+        return "index";
+    }
+}
