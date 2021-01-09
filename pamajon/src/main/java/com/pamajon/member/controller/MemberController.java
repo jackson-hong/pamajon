fd376f1f703b6aa7376dbc69c26e15694ab147b3
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-09 15:31:42 +0900

    add:header.html, footer.html, template.html

--- pamajon/src/main/java/com/pamajon/member/controller/MemberController.java
+++ pamajon/src/main/java/com/pamajon/member/controller/MemberController.java
@@ -1,4 +1,13 @@
 package com.pamajon.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
+
+    @RequestMapping("/member/myPage")
+    public String myPage(){
+        return "/member/myPage";
+    }
 }
