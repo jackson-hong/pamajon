72d0eef52231d1eefee42ce4d461edfd0a22cce0
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-13 13:38:03 +0900

    add:login.html

--- pamajon/src/main/java/com/pamajon/member/controller/MemberController.java
+++ pamajon/src/main/java/com/pamajon/member/controller/MemberController.java
@@ -3,11 +3,22 @@ package com.pamajon.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class MemberController {
 
     @RequestMapping("/member/myPage")
     public String myPage(){
         return "/member/myPage";
+    }
+
+    @RequestMapping("/member/login")
+    public String login(){
+        return "/member/login";
+    }
+
+    @RequestMapping("/member/join")
+    public String join(){
+        return "/member/join";
     }
 }
