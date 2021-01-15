e31070cf5a54806b532c8f75ae46f24e1ce07f73
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-15 12:44:44 +0900

    join.html updated by Jackson

--- pamajon/src/main/java/com/pamajon/board/controller/BoardController.java
+++ pamajon/src/main/java/com/pamajon/board/controller/BoardController.java
@@ -1,4 +1,7 @@
 package com.pamajon.board.controller;
 
+import com.pamajon.board.model.vo.Board;
+
 public class BoardController {
+    Board b = new Board(3,"name");
 }
--- pamajon/src/main/java/com/pamajon/board/model/vo/Board.java
+++ pamajon/src/main/java/com/pamajon/board/model/vo/Board.java
@@ -1,4 +1,10 @@
 package com.pamajon.board.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Data
 public class Board {
+    public int age;
+    public String name;
 }
