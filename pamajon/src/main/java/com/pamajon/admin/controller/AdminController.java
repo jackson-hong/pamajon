2a8652f9dbdddeb8852deb66578dfda63f09351a
PatrickYoo <yhy1045@naver.com>
2021-01-17 00:20:31 +0900

    admin mapping names and path a little bit modified

--- pamajon/src/main/java/com/pamajon/admin/controller/AdminController.java
+++ pamajon/src/main/java/com/pamajon/admin/controller/AdminController.java
@@ -4,37 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping(value="/admin")
 @Controller
 public class AdminController {
 
-    @GetMapping("/admin")
+    @GetMapping("/mainPage")
     public String gotoAdmin(){
 
-        return"admin/adminMainPage";
+        return"/admin/adminMainPage";
 
     }
     @GetMapping("/bResgistration")
     public String gotoResgistration(){
 
-        return "admin/profile";
+        return "/admin/profile";
     }
 
     @GetMapping("/memberManager")
     public String gotoMemberHandler(){
 
-        return "admin/memberHandler";
+        return "/admin/memberHandler";
     }
 
     @GetMapping("/productManager")
     public String gotoProductHandler(){
 
-        return "admin/productHandler";
+        return "/admin/productHandler";
     }
 
     @GetMapping("/chatroom")
     public String gotoChatroom(){
 
-        return "admin/chattingRoom";
+        return "/admin/chattingRoom";
     }
 
 
