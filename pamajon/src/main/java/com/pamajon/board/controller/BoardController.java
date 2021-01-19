ac7735d7eb564a00b98240ca2da3eedb63c9c3d6
Ethan_kim <hey0507@gmail.com>
2021-01-20 02:57:09 +0900

    add ReviewWriteform JG

--- pamajon/src/main/java/com/pamajon/board/controller/BoardController.java
+++ pamajon/src/main/java/com/pamajon/board/controller/BoardController.java
@@ -1,7 +1,17 @@
 package com.pamajon.board.controller;
 
-import com.pamajon.board.model.vo.Board;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class BoardController {
-    Board b = new Board(3,"name");
+
+    @RequestMapping("/board/productQnA")
+    public String qna(){return "/board/productQnA"; }
+    @RequestMapping("/board/productQnAwrite")
+    public String qnaWrite(){ return "/board/productQnAWrite"; }
+    @RequestMapping("/board/review")
+    public String review(){ return "/board/review"; }
+    @RequestMapping("/board/reviewWrite")
+    public String reviewWrite(){ return "/board/reviewWrite"; }
 }
