c947d5ea770e6152ac91ba8b7cf30d40b4fee9fa
Ethan_kim <hey0507@gmail.com>
2021-01-25 07:07:00 +0900

    Modify MappingRoot and create all files JG

--- pamajon/src/main/java/com/pamajon/board/controller/BoardController.java
+++ pamajon/src/main/java/com/pamajon/board/controller/BoardController.java
@@ -5,15 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
+    //Q&A관련
+    @RequestMapping("/board/qna")
+    public String qna(){return "/board/qna"; }
+    @RequestMapping("/board/qna/write")
+    public String qnaWrite(){ return "/board/qnaWrite"; }
+    @RequestMapping("/board/qna/secert")
+    public String qnaSecret(){ return "/board/qnaSecret"; }
 
-    @RequestMapping("/board/productQnA")
-    public String qna(){return "/board/productQnA"; }
-    @RequestMapping("/board/productQnAwrite")
-    public String qnaWrite(){ return "/board/productQnAWrite"; }
+    //비밀글일경우 거치는 페이지
+    @RequestMapping("/board/qna/view")
+    public String qnaDetail(){ return "/board/qnaDetail"; }
+    
+    //리뷰 관련
     @RequestMapping("/board/review")
     public String review(){ return "/board/review"; }
-    @RequestMapping("/board/reviewWrite")
+    @RequestMapping("/board/review/write")
     public String reviewWrite(){ return "/board/reviewWrite"; }
+    @RequestMapping("/board/review/view")
+    public String reviewDetail(){ return "/board/reviewDetail"; }
+    
+    
+    //마이페이지
     @RequestMapping("/board/boardList")
     public String listBoard(){ return "/board/boardList"; }
 }
