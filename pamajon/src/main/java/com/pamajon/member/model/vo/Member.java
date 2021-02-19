9f5d3e1c0b81aabef88d6a2539e12b0223e28fe2
Jackson Hong <jacksonhong0111@gmail.com>
2021-02-20 00:16:46 +0900

    to save jongu

---
+++ pamajon/src/main/java/com/pamajon/member/model/vo/Member.java
@@ -0,0 +1,28 @@
+package com.pamajon.member.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    int userId;
+    String memId;
+    String memPwd;
+    String memPwdcheckQ;
+    String memPwdcheckA;
+    String memName;
+    String memAddress;
+    String memTel;
+    String memPhone;
+    String memEmail;
+    String memGrade;
+    Date memDate;
+    boolean memStatus;
+    String memBirth;
+}
