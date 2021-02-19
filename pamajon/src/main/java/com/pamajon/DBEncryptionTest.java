769e2e4158ab45c30c12a8d6c12ca5161b793f9a
Ethan_kim <hey0507@gmail.com>
2021-02-20 04:33:48 +0900

    DB encryption test JG

---
+++ pamajon/src/main/java/com/pamajon/DBEncryptionTest.java
@@ -0,0 +1,22 @@
+package com.pamajon;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+public class DBEncryptionTest {
+    public static void main(String[] args) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("JEP");
+        String url = encryptor.encrypt("jdbc:mysql://pamajon.cqrt9cofij9d.us-east-2.rds.amazonaws.com/pamajon?autoReconnect=true&useSSL=false");
+        String username = encryptor.encrypt("admin");
+        String password = encryptor.encrypt("pamajon!");
+
+        System.out.println("url : " + url);
+        System.out.println("username : " + username);
+        System.out.println("password : " + password);
+
+        StandardPBEStringEncryptor encryptor1 = new StandardPBEStringEncryptor();
+        encryptor1.setPassword("JEP");
+        System.out.println(encryptor1   .decrypt(username));
+    }
+
+}
