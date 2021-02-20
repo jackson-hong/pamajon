1fbb705407a5bb0d7024b61666dc4df3ddf11b95
Ethan_kim <hey0507@gmail.com>
2021-02-20 15:48:57 +0900

    creating  DBencryption class JG

---
+++ pamajon/src/main/java/com/pamajon/common/DatabaseConfig.java
@@ -0,0 +1,56 @@
+package com.pamajon.common;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+@Configuration
+
+@MapperScan(basePackages="com.pamajon.mapper")
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+
+    GlobalPropertySource globalPropertySource;
+
+    public DatabaseConfig(GlobalPropertySource globalPropertySource) {
+        this.globalPropertySource = globalPropertySource;
+    }
+
+    @Bean
+    @Primary
+    public DataSource customDataSource() {
+        return DataSourceBuilder
+                .create()
+                .url(globalPropertySource.getUrl())
+                .driverClassName(globalPropertySource.getDriverClassName())
+                .username(globalPropertySource.getUsername())
+                .password(globalPropertySource.getPassword())
+                .build();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource customDataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(customDataSource);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath:mybatis/mapper/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
+        final SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
+        return sqlSessionTemplate;
+    }
+
+
+}
--- pamajon/src/main/java/com/pamajon/common/security/DBEncryptionTest.java
+++ pamajon/src/main/java/com/pamajon/common/security/DBEncryptionTest.java
@@ -1,4 +1,4 @@
-package com.pamajon;
+package com.pamajon.common.security;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
@@ -6,17 +6,19 @@ public class DBEncryptionTest {
     public static void main(String[] args) {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword("JEP");
+        String driverClassName = encryptor.encrypt("org.mariadb.jdbc.Driver");
         String url = encryptor.encrypt("jdbc:mysql://pamajon.cqrt9cofij9d.us-east-2.rds.amazonaws.com/pamajon?autoReconnect=true&useSSL=false");
         String username = encryptor.encrypt("admin");
         String password = encryptor.encrypt("pamajon!");
 
+        System.out.println("driverClassName : " + driverClassName);
         System.out.println("url : " + url);
         System.out.println("username : " + username);
         System.out.println("password : " + password);
 
         StandardPBEStringEncryptor encryptor1 = new StandardPBEStringEncryptor();
         encryptor1.setPassword("JEP");
-        System.out.println(encryptor1   .decrypt(username));
+        System.out.println(encryptor1.decrypt(driverClassName));
     }
 
 }
