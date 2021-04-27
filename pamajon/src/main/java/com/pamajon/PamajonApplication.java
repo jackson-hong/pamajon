b1c7b35d53f8fd7aa13bb487ae6885d2423ba190
EthanKim <hey0507@gmail.com>
2021-04-27 23:58:07 +0900

    edit pamajonApplication

--- pamajon/src/main/java/com/pamajon/PamajonApplication.java
+++ pamajon/src/main/java/com/pamajon/PamajonApplication.java
@@ -7,16 +7,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
 public class PamajonApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PamajonApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(applicationClass);
-	}
-
-	private static Class<PamajonApplication> applicationClass = PamajonApplication.class;
 }
