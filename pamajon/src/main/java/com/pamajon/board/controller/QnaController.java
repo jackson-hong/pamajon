ec3c7785181a8ea0595593cb42557ed0a5eaff08
EthanKim <hey0507@gmail.com>
2021-04-28 00:44:56 +0900

    modify createMethod QnaController

--- pamajon/src/main/java/com/pamajon/board/controller/QnaController.java
+++ pamajon/src/main/java/com/pamajon/board/controller/QnaController.java
@@ -1,6 +1,11 @@
 package com.pamajon.board.controller;
 
+import com.pamajon.board.model.service.QnaServiceImpl;
+import com.pamajon.board.model.vo.QnaDto;
+import com.pamajon.common.security.AES256Util;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @Log4j2
 @RestController
 @RequestMapping("/qna")
 public class QnaController {
+
+    private final AES256Util aes256Util;
+
+    @Qualifier("qnaServiceImpl")
+    private final QnaServiceImpl service;
+
+    public QnaController(AES256Util aes256Util,QnaServiceImpl service) {
+        this.aes256Util = aes256Util;
+        this.service =service;
+    }
+
+
 
     //Q&A관련
     @RequestMapping("/")
@@ -28,9 +47,40 @@ public class QnaController {
 
     @PostMapping("/write")
     public ModelAndView insertQna(ModelAndView mv, @RequestParam Map input, HttpServletRequest request){
-        log.info("well done");
         log.info(input);
+        String qnaTitle = (String)input.get("qnaTitle");
+        String qnaContent = (String)input.get("qnaContent");
+        String qnaSecretCheck = (String)input.get("qnaSecretCheck");
+        String qnaPwd = (String) input.get("qnaPwd");
 
+        log.info("done with get map > QNA pwd: " + qnaPwd);
+        try {
+            qnaPwd = aes256Util.encrypt(qnaPwd);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        log.info("Well Encrypt > " + qnaPwd);
+
+
+        int qnaSecret = qnaSecretCheck.equals("n") ? 0 : 1;
+
+        QnaDto createQna = new QnaDto();
+
+        createQna.setQnaTitle(qnaTitle);
+        createQna.setQnaContent(qnaContent);
+        createQna.setQnaStatus(qnaSecret);
+        createQna.setQnaPwd(qnaPwd);
+
+        log.info("Assecc to DB");
+        int result = service.createQna(createQna);
+
+        if(result==0){
+            mv.addObject("msg", "저장되지 않았습니다.");
+            mv.addObject("loc", "/qna/write");
+            mv.setViewName("common/msg");
+            return mv;
+        }
+        mv.setViewName("redirect:/qna");
         return mv;
     }
     @RequestMapping("/secret")
